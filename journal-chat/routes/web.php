b18dd41d3fdee7e64356b97017f247bc1ede6af0
HirokiTorigoe <torip.1125@gmail.com>
2022-06-13 22:00:09 +0900

    ✨ LaravelにReactを導入した

--- journal-chat/routes/web.php
+++ journal-chat/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/app', function () {
+    return view('app');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
